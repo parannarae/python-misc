74b490d786ee65cc56453b9719aadae6039da2b0
Will <parannarae@outlook.com>
2026-05-16 01:38:06 +0900

    Apply ruff and mypy

--- api_load_test/main.py
+++ api_load_test/main.py
@@ -2,7 +2,7 @@ import requests
 import concurrent.futures
 
 from datetime import date, datetime, timedelta
-from typing import Any, Callable, List, Optional, Tuple
+from typing import Any, Callable, List, Optional, Tuple, TypeVar, ParamSpec
 
 from data_template import DataTemplate
 from log_util import get_logger
@@ -16,15 +16,15 @@ NUMBER_OF_PROCESSES = 8
 #####
 
 ##### API configuration
-HOST_URL = 'http://localhost:8080/storyline-service'
-ACCESS_TOKEN = 'some.access.token'
+HOST_URL = "http://localhost:8080/storyline-service"
+ACCESS_TOKEN = "some.access.token"
 #####
 
 ##### Input data configuration
-DATA_TEMPLATE_FILENAME = 'data_sample.txt'  # file containing POST body data template
+DATA_TEMPLATE_FILENAME = "data_sample.txt"  # file containing POST body data template
 DEPENDENT_IDS = [411, 412, 413, 414, 415, 416, 417, 418, 419, 420]
-START_DATE =  date(2000, 3, 1)
-DAYS_TO_ITERATE = 300   # simulated site size (days are used instead of multiple sites to minimize data preparation)
+START_DATE = date(2000, 3, 1)
+DAYS_TO_ITERATE = 300  # simulated site size (days are used instead of multiple sites to minimize data preparation)
 #####
 
 ##############################
@@ -38,26 +38,34 @@ NUMBER_OF_THREADS = NUMBER_OF_PROCESSES + 4
 
 API_HEADERS = {
     "Authorization": f"Bearer {ACCESS_TOKEN}",
-    "Content-Type": 'application/json',
+    "Content-Type": "application/json",
 }
 
 
 logger = get_logger(__name__)
 
 
+# for generic typing
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
 # Note: Only top level function can be passed to ProcessPool due to limitation of pickle (i.e. wrapper function cannot apply to functions called by ProcessPoolExecutor)
 #   ref: https://stackoverflow.com/questions/72766345/attributeerror-cant-pickle-local-object-in-multiprocessing/72776044#72776044
-def log_process_time(func: Callable) -> None:
+def log_process_time(func: Callable[P, R]) -> Callable[P, R]:
     """Decorator to log processing time of a `func`
 
     Args:
         func (Callable): function to log
     """
+
     def wrapper(*args: Any, **kwargs: Any) -> Any:
         start = datetime.now()
         result = func(*args, **kwargs)
         end = datetime.now()
-        logger.info(f"{func.__name__} is executed for {(end - start).total_seconds() * 1000:.3f}ms")
+        logger.info(
+            f"{func.__name__} is executed for {(end - start).total_seconds() * 1000:.3f}ms"
+        )
 
         return result
 
@@ -71,22 +79,22 @@ def create_storyline(dependent_id: int, target_date: date) -> Optional[int]:
     """
     url = f"{HOST_URL}/v3/internal/storylines"
     response = requests.post(
-        url = url,
-        headers = API_HEADERS,
-        json = {
-            "date": target_date.strftime('%Y-%m-%d'),
+        url=url,
+        headers=API_HEADERS,
+        json={
+            "date": target_date.strftime("%Y-%m-%d"),
             "dependentId": dependent_id,
-        }
+        },
     )
 
     storyline_id = None
     if response.status_code == 201:
-        storyline_id = response.json()['data']['id']
+        storyline_id = response.json()["data"]["id"]
 
     return storyline_id
 
 
-def create_stories(storyline_id:int, json: Any) -> Tuple[int, float]:
+def create_stories(storyline_id: int, json: Any) -> Tuple[int, float]:
     """Call API to create stories under `storyline_id`.
 
     Return a tuple containing status code and a processing time in second.
@@ -96,18 +104,16 @@ def create_stories(storyline_id:int, json: Any) -> Tuple[int, float]:
 
     url = f"{HOST_URL}/v3/internal/storylines/{storyline_id}/stories/create-bulk"
 
-    response = requests.post(
-        url = url,
-        headers = API_HEADERS,
-        json = json
-    )
+    response = requests.post(url=url, headers=API_HEADERS, json=json)
 
     end = datetime.now()
 
     return response.status_code, (end - start).total_seconds()
 
 
-def create_storyline_data(dependent_id: int, target_date: date, data_template: DataTemplate) -> Tuple[Optional[int], float]:
+def create_storyline_data(
+    dependent_id: int, target_date: date, data_template: DataTemplate
+) -> Tuple[Optional[int], float | None]:
     """Call create storyline and stories API in sequence.
 
     Return a tuple containing storyline_id (None if failed on creating) and
@@ -118,22 +124,25 @@ def create_storyline_data(dependent_id: int, target_date: date, data_template: D
 
     if storyline_id:
         code, process_time = create_stories(
-            storyline_id = storyline_id,
-            json = data_template.get_json(
-                dependent_id = dependent_id,
-                target_date = target_date.strftime('%Y-%m-%d')
-            )
+            storyline_id=storyline_id,
+            json=data_template.get_json(
+                dependent_id=dependent_id, target_date=target_date.strftime("%Y-%m-%d")
+            ),
         )
 
         if code != 201:
             logger.info(f"Fail on creating stories for storyline (id:{storyline_id})")
     else:
-        logger.info(f"Fail on creating storyline for dependent ({dependent_id}) at {target_date}")
+        logger.info(
+            f"Fail on creating storyline for dependent ({dependent_id}) at {target_date}"
+        )
 
     return storyline_id, process_time
 
 
-def run_process(target_date: date, data_template: DataTemplate) -> List[Tuple[Optional[int], float]]:
+def run_process(
+    target_date: date, data_template: DataTemplate
+) -> List[Tuple[Optional[int], float | None]]:
     """Create storyline for all dependents at `target_date`.
 
     Return a list containing tuple of created storyline id (None if failed on
@@ -143,15 +152,24 @@ def run_process(target_date: date, data_template: DataTemplate) -> List[Tuple[Op
         [(1, 0.53), (2, 0.50), (3, 0.49)]
     """
     logger.info(f"===Running threads to create storyline at {target_date}===")
-    with concurrent.futures.ThreadPoolExecutor(max_workers=NUMBER_OF_THREADS) as executor:
-        futures = [executor.submit(create_storyline_data, dependent_id, target_date, data_template) for dependent_id in DEPENDENT_IDS]
+    with concurrent.futures.ThreadPoolExecutor(
+        max_workers=NUMBER_OF_THREADS
+    ) as executor:
+        futures = [
+            executor.submit(
+                create_storyline_data, dependent_id, target_date, data_template
+            )
+            for dependent_id in DEPENDENT_IDS
+        ]
 
         result = [f.result() for f in concurrent.futures.as_completed(futures)]
 
     return result
 
 
-def __extract_result(results: List[List[Tuple[int, float]]]) -> Tuple[List[List[int]], List[float]]:
+def __extract_result(
+    results: List[List[Tuple[int | None, float | None]]],
+) -> Tuple[List[List[int]], List[float]]:
     """From `result` extract a nested list of storyline ids, and a flat list of process time.
 
     Return value example:
@@ -181,8 +199,13 @@ def main() -> List[List[int]]:
     date_list = [START_DATE + timedelta(days=i) for i in range(DAYS_TO_ITERATE)]
     data_template = DataTemplate(DATA_TEMPLATE_FILENAME)
 
-    with concurrent.futures.ProcessPoolExecutor(max_workers=NUMBER_OF_PROCESSES) as executor:
-        futures = [executor.submit(run_process, target_date, data_template) for target_date in date_list]
+    with concurrent.futures.ProcessPoolExecutor(
+        max_workers=NUMBER_OF_PROCESSES
+    ) as executor:
+        futures = [
+            executor.submit(run_process, target_date, data_template)
+            for target_date in date_list
+        ]
         results = [f.result() for f in concurrent.futures.as_completed(futures)]
 
     number_of_storylines = DAYS_TO_ITERATE * len(DEPENDENT_IDS)
@@ -190,14 +213,14 @@ def main() -> List[List[int]]:
 
     storyline_ids, stories_creation_time_list = __extract_result(results)
 
-    average_time_str = 'N/A'
+    average_time_str = "N/A"
     if len(stories_creation_time_list) > 0:
         # calculate average time in ms
-        average_time_str = f'{sum(stories_creation_time_list) / number_of_storylines * 1000:.3f}'
+        average_time_str = (
+            f"{sum(stories_creation_time_list) / number_of_storylines * 1000:.3f}"
+        )
 
-    logger.info(
-        f"Average time on the creating stories API: {average_time_str}ms"
-    )
+    logger.info(f"Average time on the creating stories API: {average_time_str}ms")
 
     return storyline_ids
 
@@ -208,10 +231,7 @@ def delete_storyline(id: int) -> Optional[int]:
     Return `id` if it failed to delete.
     """
     url = f"{HOST_URL}/v3/internal/storylines/{id}/unsafe-delete"
-    response = requests.post(
-        url = url,
-        headers = API_HEADERS
-    )
+    response = requests.post(url=url, headers=API_HEADERS)
 
     if response.status_code != 204:
         return id
@@ -222,7 +242,9 @@ def run_delete_process(storyline_ids: List[int]) -> List[int]:
 
     Returns list of storyline ids failed on deletion.
     """
-    with concurrent.futures.ThreadPoolExecutor(max_workers=NUMBER_OF_THREADS) as executor:
+    with concurrent.futures.ThreadPoolExecutor(
+        max_workers=NUMBER_OF_THREADS
+    ) as executor:
         futures = [executor.submit(delete_storyline, id) for id in storyline_ids]
         results = [f.result() for f in concurrent.futures.as_completed(futures)]
 
@@ -230,21 +252,26 @@ def run_delete_process(storyline_ids: List[int]) -> List[int]:
 
 
 @log_process_time
-def clean_up(storyline_ids_list:List[List[int]]) -> None:
-    """Delete storylines with id in `storyline_ids_list`
-    """
-    with concurrent.futures.ProcessPoolExecutor(max_workers=NUMBER_OF_THREADS) as executor:
-        futures = [executor.submit(run_delete_process, lst) for lst in storyline_ids_list]
+def clean_up(storyline_ids_list: List[List[int]]) -> None:
+    """Delete storylines with id in `storyline_ids_list`"""
+    with concurrent.futures.ProcessPoolExecutor(
+        max_workers=NUMBER_OF_THREADS
+    ) as executor:
+        futures = [
+            executor.submit(run_delete_process, lst) for lst in storyline_ids_list
+        ]
         results = [f.result() for f in concurrent.futures.as_completed(futures)]
 
     # results is a list containing list of ids returned from each process
-    logger.info(f"All storylines are deleted except ids: {[item for result in results for item in result]}")
+    logger.info(
+        f"All storylines are deleted except ids: {[item for result in results for item in result]}"
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # for i in range(5):
     #     logger.info(f"==========Iteration: {i}==========")
     results = main()
     # clean up created storylines
-    logger.info(f"=====Deleting storylines=====")
+    logger.info("=====Deleting storylines=====")
     clean_up(results)
